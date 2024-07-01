73107d080e51458ed52e19b088635a995ccc81ea
Raddames <raddamestonui48@gmail.com>
2024-07-02 00:11:34 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,9 +1,7 @@
-# server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
-
 from models import db, Earthquake
 
 app = Flask(__name__)
@@ -14,14 +12,47 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
-    return make_response(body, 200)
+    return make_response(jsonify(body), 200)
 
-# Add views here
+# Task #3: Add view to get an earthquake by id
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        body = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    
+    return make_response(jsonify(body), status)
 
+# Task #4: Add view to get earthquakes matching a minimum magnitude value
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    quakes = [
+        {
+            'id': quake.id,
+            'location': quake.location,
+            'magnitude': quake.magnitude,
+            'year': quake.year
+        }
+        for quake in earthquakes
+    ]
+    body = {
+        'count': len(quakes),
+        'quakes': quakes
+    }
+    return make_response(jsonify(body), 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -3,7 +3,17 @@ from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData()
-
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = 'earthquakes'
+
+    id = db.Column(db.Integer, primary_key=True)  # Corrected primary_key argument
+    magnitude = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String(64), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    # The __repr__ method in Python is a special method used to define the "official" string representation of an object. 
+    def __repr__(self):
+        return f'<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>'
